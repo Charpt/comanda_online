b1733b42693e7c7638dc364c4b7b517e0bda5afc
Charpt <felipe.lima.sg@gmail.com>
2025-03-17 23:47:49 -0300

    lista de compras correcoes ao deletar produto e atualizar

--- pages/lista_de_compras/lista_de_compras.js
+++ pages/lista_de_compras/lista_de_compras.js
@@ -84,7 +84,10 @@ dados.forEach(dados => {
 
 
 
-
+var atualizar_ou_cadastrar = true;
+var uid;
+var item_nome_value;
+var item_quant_value;
 function Criar_Colunas_Lista_de_Compras(lista_compras){
     
 
@@ -111,9 +114,11 @@ function Criar_Colunas_Lista_de_Compras(lista_compras){
 }
 
 const checkbox_deletar = document.getElementById('checkbox_deletar_id');
+const checkbox_editar = document.getElementById('checkbox_editar_id');
+const btn_deletar = document.getElementById('btn_deletar_id');
 function add_item_lista_de_compras(lista_compras, dados,numero_item){
     
-    var uid = dados.uid;
+     uid = dados.uid;
 
 
     
@@ -132,7 +137,7 @@ function add_item_lista_de_compras(lista_compras, dados,numero_item){
     td_item_nome.innerHTML = dados.item_nome;
     td_item_nome.classList.add('td_item_nome');
     td_item_nome.id ='td_item_nome_id'+numero_item;
-    console.log(td_item_nome.id);
+    //console.log(td_item_nome.id);
     tr_tbody.appendChild(td_item_nome); 
 
 
@@ -141,7 +146,7 @@ function add_item_lista_de_compras(lista_compras, dados,numero_item){
     
 
     const td_item_quant = document.createElement('td');
-    td_item_quant.innerHTML = dados.item_quantidade;
+    td_item_quant.innerHTML = dados.item_quant;
     td_item_quant.classList.add('td_item_quant');
     td_item_quant.id ='td_item_quant_id'+numero_item;
     tr_tbody.appendChild(td_item_quant);
@@ -154,41 +159,60 @@ function add_item_lista_de_compras(lista_compras, dados,numero_item){
     td_item_checkbox.classList.add('td_item_checkbox');
     td_item_checkbox.id ='item_checkbox_id'+numero_item;
     
-
+var editar_desativado = true;
     
+// se a opçao deletar checkbox estiver ativa 
+checkbox_editar.addEventListener('change', function() {
+    if (this.checked) { // Verifica se a checkbox está marcada
+        
+        
+        
+        document.getElementById('btn_deletar_id').style.display = 'inline';
 
-    checkbox_deletar.addEventListener('change', function() {   
-        if (this.checked) { // Verifica se a checkbox está marcada
-            td_item_nome.style.backgroundColor = 'rgb(117, 0, 0)';
-            td_item_quant.style.backgroundColor = 'rgb(117, 0, 0)';
-            td_item_checkbox.style.backgroundColor = 'rgb(117, 0, 0)';
+        td_item_nome.style.backgroundColor = 'rgb(202, 199, 29)';
+            td_item_quant.style.backgroundColor = 'rgb(202, 199, 29)';
+            td_item_checkbox.style.backgroundColor = 'rgb(202, 199, 29)';
     
             input_item_checkbox.style.display = 'none';
-    
-            // Função de callback para o evento de clique
-            const handleClick = function(Event) {
-                console.log('oie');
-                Deseja_Deletar_Produto_carrinho_de_compras('lista_de_compra',dados.uid,uid);
-                
-            };
-    
-            // Adiciona o listener de clique
-            td_item_nome.addEventListener('click', handleClick);
-    
-            // Armazena a função de callback para poder removê-la depois
-            td_item_nome._handleClick = handleClick;
-    
-        } else {
 
-            input_item_checkbox.style.display = '';
+        // Função de callback para o evento de clique
+        const handleClick = function(Event) {
+            
+            form.item_nome().value = document.getElementById('td_item_nome_id'+numero_item).innerHTML;
+            form.item_quant().value = document.getElementById('td_item_quant_id'+numero_item).innerHTML;
+            form.uid().innerHTML = dados.uid;
+
+            btn_deletar.onclick= function(){
+               if(form.uid().innerHTML != ''){
+                Deseja_Deletar_Produto_carrinho_de_compras('lista_de_compra',dados.uid,uid);
+               }
+            }
+            
+                      ToggleCadastrarItemButton();
+                      atualizar_ou_cadastrar =false;
+            
+        };
+
+        // Adiciona o listener de clique
+        td_item_nome.addEventListener('click', handleClick);
+
+        // Armazena a função de callback para poder removê-la depois
+        td_item_nome._handleClick = handleClick;
+
+    } else {
+
+
+
+                    input_item_checkbox.style.display = '';
                 // faz esperar um tempo antes de execultar o codigo dentro dele no caso ele espera um tempo antes de ir apra a proxima pagina
     setTimeout(() => {
 
         location.reload();
 
     }, 1000); // 1000 milissegundos = 1 segundos
-        }
-    });
+
+    }
+ });
 
 
 if(dados.item_checkbox == true){
@@ -277,8 +301,12 @@ function Deletar_Produto_carrinho_de_compras(colecao,dados,elemento_remove){
     dados_servicos.Delete_item(colecao,dados)
    .then(snapshot =>{
 
-    console.log(elemento_remove);
     document.getElementById(elemento_remove).remove();
+    form.item_nome().value ='';
+    form.item_quant().value ='';
+    form.uid().innerHTML ='';
+    atualizar_ou_cadastrar = true;
+
     
     removeLoading();
 
@@ -294,12 +322,27 @@ function Deletar_Produto_carrinho_de_compras(colecao,dados,elemento_remove){
 
 
 
-
+// botao para cadastrar item no banco de dados
 const btn_add = document.getElementById('botao_colocar_na_lista_id');
 btn_add.onclick = function() {
 
+
+
+
     ShowLoading();
-    salvar_item_lista_de_compra();
+    if(atualizar_ou_cadastrar){
+
+        salvar_item_lista_de_compra();
+    }else{
+
+        const dados ={
+            item_nome: form.item_nome().value,
+            item_quant: form.item_quant().value
+        };
+        
+        Atualizar_dados_do_item('lista_de_compra', form.uid().innerHTML,dados);
+    }
+
     // faz esperar um tempo antes de execultar o codigo dentro dele no caso ele espera um tempo antes de ir apra a proxima pagina
     setTimeout(() => {
 
@@ -334,7 +377,7 @@ function Obter_dados_Lista_de_compras() {
 
     
     dados['item_nome'] = document.getElementById('item_nome_id').value;
-    dados['item_quantidade'] = document.getElementById('item_quant_id').value;
+    dados['item_quant'] = document.getElementById('item_quant_id').value;
     dados['status'] = 'ativo';
 
     dados['item_checkbox'] = false;
@@ -351,13 +394,28 @@ function Obter_dados_Lista_de_compras() {
 function Cadastrar_Pedido(dados){
     dados_servicos.Salvar_no_Banco_Dados(dados,"lista_de_compra")
     .then(()=>{
-
+        removeLoading();
     }).catch(()=>{
        removeLoading();
-       alert("Erro ao salvar Produto");
+       alert("Erro ao salvar Item");
     })
 }
 
+/******************************************************
+ * FUNCAO PARA ATUALIZAR OS DADOS CADASTRADOS DOS ITENS 
+ ********************************************************/
+function Atualizar_dados_do_item(colecao,doc,dados){
+
+    ShowLoading();
+    dados_servicos.Atualizar_dados(colecao,doc,dados)
+     .then(()=>{
+
+     }).catch(()=>{
+        removeLoading();
+        alert("Erro ao atualizar Item");
+     })
+
+}
 
 // validar cadastro
 
@@ -371,7 +429,7 @@ function OnChange_Item_nome(){
 function OnChange_Item_quant(){
     const item_quant = form.item_quant().value;
     form.item_quant_Obrigatorio().style.display = item_quant ? "none":"block";
-    form.item_quant_Invalido().style.display = item_quant  > 0? "none": "block";
+    form.item_quant_Invalido().style.display = item_quant  => 0? "none": "block";
 
     ToggleCadastrarItemButton();
 }
@@ -389,7 +447,7 @@ function isFromValid(){
     }
 
     const item_quant = form.item_quant().value;
-    if(!item_quant || item_quant <= 0){
+    if(!item_quant || item_quant < 0){
         return false;
     }
 
@@ -416,6 +474,7 @@ const form = {
 
     btnCadastrar:() => document.getElementById('botao_colocar_na_lista_id'),
 
+    uid:() => document.getElementById('uid_id'),
    
     
 }
